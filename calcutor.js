c6399118e13d2edd56eb78747a9c78493b4ae28f
Ebru Şara Bağca <75140575+ebrusarabgc@users.noreply.github.com>
2021-06-07 14:42:09 +0300

    Update calcutor.js

--- calcutor.js
+++ calcutor.js
@@ -17,7 +17,7 @@ function clr() {
 }
 
 function del() {
-    var exp = document.formCal.textview.value;
+    var exp = document.getElementById("textview").value;
     document.getElementById("textview").value = exp.substring(0, exp.length - 1);
 }
 
